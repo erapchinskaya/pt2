a682768ae845ea47d9376973ae82c8aa3159c719
erapchinskaya <e.rapchinskaya@gmail.com>
2025-11-05 14:57:20 +0300

    add files

---
+++ battle city/water block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="water block" tilewidth="40" tileheight="40" tilecount="1" columns="1">
+ <image source="../../питон 2-20250909T140909Z-1-001/питон 2/4. battle city – начало/img/water block.png" width="40" height="40"/>
+</tileset>
