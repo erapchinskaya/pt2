bb90f1cace861bd40b9a7340448bb241401c88b9
erapchinskaya <e.rapchinskaya@gmail.com>
2026-01-17 12:26:22 +0300

    1111

---
+++ platformer/set/platformer_set.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="platformer_set" tilewidth="80" tileheight="80" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../images/box.png" width="80" height="80"/>
+ </tile>
+ <tile id="1">
+  <image source="../images/center.png" width="80" height="80"/>
+ </tile>
+ <tile id="2">
+  <image source="../images/earth.png" width="80" height="80"/>
+ </tile>
+ <tile id="4">
+  <image source="../images/water.png" width="80" height="80"/>
+ </tile>
+</tileset>
